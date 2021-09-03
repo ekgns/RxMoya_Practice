a0e58b33c0f0295536795ee5779fca71daa76264
kimdahun.hello <kimdahun.hello@gmail.com>
2021-09-03 22:49:15 +0900

    프로젝트 생성

---
+++ RxMoya_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxMoya_practice
+//
+//  Created by 다훈김 on 2021/09/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RxMoya_practiceTests/RxMoya_practiceTests.swift
@@ -0,0 +1,33 @@
+//
+//  RxMoya_practiceTests.swift
+//  RxMoya_practiceTests
+//
+//  Created by 다훈김 on 2021/09/03.
+//
+
+import XCTest
+@testable import RxMoya_practice
+
+class RxMoya_practiceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
